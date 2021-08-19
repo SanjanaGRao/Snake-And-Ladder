02223b7e74d268a96db68a466749111a502ba356
Sanjana Rao <sanjanagrao99@gmail.com>
2021-08-19 15:00:32 +0530

    Added: Welcome Message

---
+++ SnakeAndLadderCode.java
@@ -0,0 +1,10 @@
+
+public class SnakeAndLadderCode {
+
+	public static void main(String[] args) {
+		//intialize position and player
+		int Player_No = 1;
+		int position = 0;
+		System.out.println("Welcome to Snake & Ladder");
+	}
+}
