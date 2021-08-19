e9ea98f3ddd7792a2595b65b29ce522d02e585f0
Sanjana Rao <sanjanagrao99@gmail.com>
2021-08-19 15:06:11 +0530

    Added: Snake and Ladder UC2

--- SnakeAndLadderCode.java
+++ SnakeAndLadderCode.java
@@ -5,8 +5,15 @@ public class SnakeAndLadderCode {
 		//intialize position and player
 		int Player_No = 1;
 		int position = 0;
+		int MinDie=1; // to implement random function
+		int MaxDie=6;
+		
+		int die = (int) Math.floor(Math.random()*(MaxDie-MinDie+1)+MinDie);
+		
 		System.out.println("Welcome to Snake & Ladder");
 		System.out.println(" No. of players: " + Player_No);
-		System.out.println(" Initial Poition: " + position);
+		System.out.println(" Initial Position: " + position);
+		System.out.println(" The value of the die: " + die);
+		
 	}
 }
